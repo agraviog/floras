1ca253c26ab62bdbbce501ffe064e0908ca8e88a
Genesis Agravio <genesisagravio@Genesiss-MacBook-Air.local>
2024-10-11 09:34:26 +0800

    added global

--- global-script.js
+++ global-script.js
@@ -1,3 +1,4 @@
+// store swiper
 const swiperStore = new Swiper('.swiper.is-store', {
   slidesPerView: 3.2,
   slidesPerGroup: 1,
@@ -32,3 +33,35 @@ const swiperStore = new Swiper('.swiper.is-store', {
     },
   },
 });
+
+// nav button function
+$('.nav-btn-wrap').on('click', function () {
+  $('.nav-wrap').toggleClass('u-color-dark');
+});
+
+$('.button').each(function () {
+  var $button = $(this);
+  var $buttonIcon = $button.find('.button-icon');
+
+  $button.on('mouseenter', function () {
+    $buttonIcon.addClass('rotate');
+  });
+
+  $button.on('mouseleave', function () {
+    $buttonIcon.removeClass('rotate');
+  });
+});
+
+// card store function
+$('.card-store').hover(
+  function () {
+    $(this).find('.store-name').css('text-decoration', 'underline');
+    $(this).find('.store-name').css('text-decoration-thickness', '2px');
+    $(this).find('img').css('transform', 'scale(1.2)');
+  },
+  function () {
+    $(this).find('.store-name').css('text-decoration', 'none');
+    $(this).find('.store-name').css('text-decoration-thickness', '');
+    $(this).find('img').css('transform', 'scale(1)');
+  }
+);
